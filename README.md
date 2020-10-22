82feb2301a6a0ee619dd1d6bbbfd89f81aa9a8b6
u-blavins <usama.blavins1@gmail.com>
2020-10-23 00:57:57 +0100

    Update README. Added main.go and completed Ch3 GolangBot

--- README.md
+++ README.md
@@ -4,3 +4,19 @@
 
 ## Learning Resource
 [golang-tutorial](https://www.educative.io/blog/golang-tutorial)
+[GOLANGBOT](https://golangbot.com/golang-tutorial-part-1-introduction-and-installation/)
+
+## Notes
+
+*Advantages of Go for server side programming*
+
+- Simple Syntax: syntax is simple and concise, not bloated with unnecessary features.
+- Easy to write concurrent programs: Concurrency is an inherent part of the language - writing multithreaded progras is a piece of cake.
+
+*Intermediate concepts of Go*
+
+Go uses flexible `for` and `switch` loops. There are also new control structures, such as type switch and `select.
+
+> `if-else`: this construct tests for a conditional statement - logical or boolean.
+> `switch-case`: structure is used instead of long `if` statements that compare variables to values.
+
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+// every Go file must start with the `package name` statement
+// packages are used to provide code reusability. The main function should
+// always reside in the main package
+
+import (
+	"fmt"
+	"math"
+)
+
+// import statement used to import other packages
+// `fmt` package is imported and used in main function to print
+
+func main() {
+	// fmt.Println("Hello World")
+	// demoVariables()
+	shortHandDec()
+}
+
+func demoVariables() {
+	// `var name type` is the syntax to declare a single variable
+	var age int = 22 // variable declaration
+	// age is initialised with a zero value '0'
+	fmt.Println("My age is", age)
+	//  if a variable has an initial value - go can infer the type
+	var anotherAge = 22
+	fmt.Println("Bar's age is", anotherAge)
+	var height, width int = 50, 100
+	fmt.Println("Width is", width, "and height is", height)
+	var (
+		name   = "Ruby"
+		animal = "Cat"
+		colour string
+	)
+	fmt.Println("My", animal, "is called", name, "and is", colour)
+}
+
+func shortHandDec() {
+	// Go provides another concise way to declare variables using ':='
+	// `name := initialvalue`
+	// count := 10
+	// fmt.Println("Count = ", count)
+	name, age := "Foo", 21
+	fmt.Println("my name is", name, "age is", age)
+	a, b := 145.8, 543.8
+	c := math.Min(a, b)
+	fmt.Println("Minimum value is", c)
+	// Go is strongly typed, `name` = 29 cannot happen
+}
